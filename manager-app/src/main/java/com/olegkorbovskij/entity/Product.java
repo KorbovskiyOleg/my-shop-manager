e7ce5df2c035492bcc446a79620b7f7b802fd64e
Car_Bit <olegkorbovskij@gmail.com>
2026-01-21 05:04:58 +0300

    Описание продукта(Entity)

---
+++ manager-app/src/main/java/com/olegkorbovskij/entity/Product.java
@@ -0,0 +1,15 @@
+package com.olegkorbovskij.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    private Integer id;
+    private String title;
+    private String details;
+}
