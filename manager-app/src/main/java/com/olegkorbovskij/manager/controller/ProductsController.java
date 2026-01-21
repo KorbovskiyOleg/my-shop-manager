9d5537a2c1b98f0f490bf11fa2f04e4ab63e2b4d
Car_Bit <olegkorbovskij@gmail.com>
2026-01-21 06:17:46 +0300

    Внесение зависимостей Описание структуры товаров

--- manager-app/src/main/java/com/olegkorbovskij/manager/controller/ProductsController.java
+++ manager-app/src/main/java/com/olegkorbovskij/manager/controller/ProductsController.java
@@ -1,8 +1,13 @@
 package com.olegkorbovskij.manager.controller;
 
 
+import com.olegkorbovskij.manager.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ProductsController {
+
+    private final ProductService productService;
 }
--- manager-app/src/main/java/com/olegkorbovskij/manager/service/DefaultProductService.java
+++ manager-app/src/main/java/com/olegkorbovskij/manager/service/DefaultProductService.java
@@ -1,7 +1,12 @@
 package com.olegkorbovskij.manager.service;
 
+import com.olegkorbovskij.manager.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DefaultProductService implements ProductService {
+
+    private final ProductRepository productRepository;
 }
