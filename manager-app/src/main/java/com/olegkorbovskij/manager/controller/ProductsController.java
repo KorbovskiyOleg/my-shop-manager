60254a4fb3699c047ac552e72509b31bff544f24
Car_Bit <olegkorbovskij@gmail.com>
2026-01-21 06:04:43 +0300

    Регистрация компонентов в контексте приложения

--- manager-app/src/main/java/com/olegkorbovskij/manager/controller/ProductsController.java
+++ manager-app/src/main/java/com/olegkorbovskij/manager/controller/ProductsController.java
@@ -1,4 +1,8 @@
 package com.olegkorbovskij.manager.controller;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ProductsController {
 }
--- manager-app/src/main/java/com/olegkorbovskij/manager/repository/InMemoryProductRepository.java
+++ manager-app/src/main/java/com/olegkorbovskij/manager/repository/InMemoryProductRepository.java
@@ -1,4 +1,8 @@
 package com.olegkorbovskij.manager.repository;
 
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class InMemoryProductRepository implements ProductRepository {
 }
